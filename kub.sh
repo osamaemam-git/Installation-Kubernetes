9b3fab387752d417a24c4440902d3a97081ce98b
Patrick Luzolo <eldorplus@gmail.com>
2023-10-10 10:14:39 +0200

    Update url (#1)
    * Add .whitesource configuration file
    
    * Update kub.sh
    
    ---------
    
    Co-authored-by: whitesource-bolt-for-github[bot] <42819689+whitesource-bolt-for-github[bot]@users.noreply.github.com>

--- kub.sh
+++ kub.sh
@@ -22,7 +22,7 @@ mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config 
 sudo chown $(id -u):$(id -g) $HOME/.kube/config  
 #On récupère les nods puis les appliquent 
-sudo kubetcl apply -f https://raw;githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml 
+sudo kubetcl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml 
 sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/k8s-manifests/kube-flannel-rbac.yml 
 sudo kubectl get pods --all-namespaces 
 sudo apt-get update
